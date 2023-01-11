f5a2ae755e2d4260c2e810b6f8c0ebfc0653a35e
tekater <tekateplay1@yandex.ru>
2023-01-11 19:14:31 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String regex = "ale";
+        System.out.println("ale - " + "ale".matches(regex));
+        ////
+        String regex2 ="[Java][7-9]";
+        System.out.println("j7 - " + "j7".matches(regex2));
+        System.out.println("Java8 - " + "Java8".matches(regex2));
+        System.out.println("J9 - " + "J9".matches(regex2));
+        System.out.println("v8 - " + "v8".matches(regex2));
+        ////
+        
+    }
+}
