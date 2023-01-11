3d5eae25aeb31a428699b93351694b0c511c2dee
tekater <120014627+tekater@users.noreply.github.com>
2023-01-11 19:32:22 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
 public class Main {
     public static void main(String[] args) {
         String regex = "ale";
-        System.out.println("ale - " + "ale".matches(regex));
+
+        System.out.println("ale" + " - " + "ale".matches(regex));
         System.out.println(" ");
         ////
-        String regex2 ="[Java][7-9]";
-        System.out.println("j7 - " + "j7".matches(regex2));
-        System.out.println("Java8 - " + "Java8".matches(regex2));
-        System.out.println("J9 - " + "J9".matches(regex2));
-        System.out.println("v8 - " + "v8".matches(regex2));
+        String regex1 ="[Java][7-9]";
+
+        System.out.println("Java :");
+        System.out.println("j7" + " - " + "j7".matches(regex1));
+        System.out.println("Java8" + " - " + "Java8".matches(regex1));
+        System.out.println("J9" + " - " + "J9".matches(regex1));
+        System.out.println("v8" + " - " + "v8".matches(regex1));
+        System.out.println(" ");
+        ////
+        String regex0 ="[Java]{4}[7-9]";
+
+        System.out.println("Java + захват {4} :");
+        System.out.println("j7" + " - " + "j7".matches(regex0));
+        System.out.println("Java8" + " - " + "Java8".matches(regex0));
+        System.out.println("J9" + " - " + "J9".matches(regex0));
+        System.out.println("v8" + " - " + "v8".matches(regex0));
+        System.out.println(" ");
+        ////
+        String regex2 = "ab.?";
+        String regex3 = "abc{0,1}";
+
+        System.out.println("abc" + " - " + "abc".matches(regex2));
+        System.out.println("abc" + " - " + "abc".matches(regex3));
+        System.out.println(" ");
+        ////
+        // Квантификатор +
+        String regexPlus = "ca+b";
+
+        System.out.println("Квантификатор + :");
+        System.out.println("cab" + " - " + "cab".matches(regexPlus));
+        System.out.println("caaaaab" + " - " + "caaaaab".matches(regexPlus));
+        System.out.println("cb" + " - " + "cb".matches(regexPlus));
+        System.out.println(" ");
+        // Квантификатор *
+        String regexMul = "ca*b";
+
+        System.out.println("Квантификатор * :");
+        System.out.println("cab" + " - " + "cab".matches(regexMul));
+        System.out.println("caaaaab" + " - " + "caaaaab".matches(regexMul));
+        System.out.println("cb" + " - " + "cb".matches(regexMul));
+        System.out.println(" ");
+        ////
+
+        String regexRep2 = "1{2,3}";
+
+        System.out.println("regexRep2 :");
+        System.out.println("11" + " - " + "11".matches(regexRep2));
+        System.out.println("111" + " - " + "111".matches(regexRep2));
+        System.out.println("1" + " - " + "1".matches(regexRep2));
     }
 }
