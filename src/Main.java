ee3031a1cc45dec768df70cfc2a4a1147e0fc84e
tekater <120014627+tekater@users.noreply.github.com>
2023-01-11 20:19:15 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -104,5 +104,18 @@ public class Main {
         System.out.println("abbba" + " - " + "abbba".matches(regexRep7));
         System.out.println("abbbbba" + " - " + "abbbbba".matches(regexRep7));
         System.out.println(" ");
+        ////
+
+        String regular = "[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}";
+
+        System.out.println("regular: ");
+        System.out.println("1" + " - " + "1c1a3de4-c711-4e9c-bed2-e75fd69ce920".matches(regular));
+        System.out.println("2" + " - " + "ad9e03f0-4bd4-46db-k79e-94ca17d4b74f".matches(regular));
+        System.out.println("3" + " - " + "f087d9fc-xyz3-4a3f-93e0-bdb8d9b68268".matches(regular));
+        System.out.println("4" + " - " + "92ae7a-94f-40c-a22-de4b42dd13".matches(regular));
+        System.out.println("5" + " - " + "f66024c3_50f8_442e_9387_28746581f472".matches(regular));
+        System.out.println(" ");
+        ////
+        
     }
 }
