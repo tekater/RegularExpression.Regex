250f8a01c8833f640b0ca2cb63c81baed813db82
tekater <120014627+tekater@users.noreply.github.com>
2023-01-11 19:16:30 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,5 @@ public class Main {
         System.out.println("Java8 - " + "Java8".matches(regex2));
         System.out.println("J9 - " + "J9".matches(regex2));
         System.out.println("v8 - " + "v8".matches(regex2));
-        ////
-
     }
 }
